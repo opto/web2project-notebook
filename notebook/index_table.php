7db0dad235a7ddf553b1cfd0824190c9933a652b
Keith Casey <keith@caseysoftware.com>
2017-01-02 23:53:37 -0600

    fixing pagination

--- notebook/index_table.php
+++ notebook/index_table.php
@@ -8,91 +8,6 @@ global $AppUI, $deny1, $canRead, $canEdit;
 // Files modules: index page re-usable sub-table
 $m = 'notebook';
 
-// ****************************************************************************
-// Page numbering variables
-// Pablo Roca (pabloroca@Xmvps.org) (Remove the X)
-// 19 August 2003
-//
-// $tab             - file category
-// $page            - actual page to show
-// $xpg_pagesize    - max rows per page
-// $xpg_min         - initial record in the SELECT LIMIT
-// $xpg_totalrecs   - total rows selected
-// $xpg_sqlrecs     - total rows from SELECT LIMIT
-// $xpg_total_pages - total pages
-// $xpg_next_page   - next pagenumber
-// $xpg_prev_page   - previous pagenumber
-// $xpg_break       - stop showing page numbered list?
-// $xpg_sqlcount    - SELECT for the COUNT total
-// $xpg_sqlquery    - SELECT for the SELECT LIMIT
-// $xpg_result      - pointer to results from SELECT LIMIT
-function shownavbar_notes($xpg_totalrecs, $xpg_pagesize, $xpg_total_pages, $page)
-{
-
-	global $AppUI, $m;
-	$xpg_break = false;
-	$xpg_prev_page = $xpg_next_page = 0;
-
-	$s = '<table width="100%" cellspacing="0" cellpadding="0" border="0"><tr>';
-
-	if ($xpg_totalrecs > $xpg_pagesize) {
-		$xpg_prev_page = $page - 1;
-		$xpg_next_page = $page + 1;
-		// left buttoms
-		if ($xpg_prev_page > 0) {
-			$s .= '<td align="left" width="15%"><a href="./index.php?m=' . $m . 'notebook&amp;page=1"><img src="' . w2PfindImage('navfirst.gif') . '" border="0" Alt="First Page"></a>&nbsp;&nbsp;';
-			$s .= '<a href="./index.php?m=' . $m . '&amp;page=' . $xpg_prev_page . '"><img src="' . w2PfindImage('navleft.gif') . '" border="0" Alt="Previous page (' . $xpg_prev_page . ')"></a></td>';
-		} else {
-			$s .= '<td width="15%">&nbsp;</td>';
-		}
-
-		// central text (files, total pages, ...)
-		$s .= '<td align="center" width="70%">' . $xpg_totalrecs . ' ' . $AppUI->_('Note(s)') . ' (' . $xpg_total_pages . ' ' . $AppUI->_('Page(s)') . ')</td>';
-
-		// right buttoms
-		if ($xpg_next_page <= $xpg_total_pages) {
-			$s .= '<td align="right" width="15%"><a href="./index.php?m=' . $m . '&amp;page=' . $xpg_next_page . '"><img src="' . w2PfindImage('navright.gif') . '" border="0" Alt="Next Page (' . $xpg_next_page . ')"></a>&nbsp;&nbsp;';
-			$s .= '<a href="./index.php?m=' . $m . '&amp;page=' . $xpg_total_pages . '"><img src="' . w2PfindImage('navlast.gif') . '" border="0" Alt="Last Page"></a></td>';
-		} else {
-			$s .= '<td width="15%">&nbsp;</td></tr>';
-		}
-		// Page numbered list, up to 30 pages
-		$s .= '<tr><td colspan="3" align="center"> [ ';
-
-		for ($n = $page > 16 ? $page - 16 : 1; $n <= $xpg_total_pages; $n++) {
-			if ($n == $page) {
-				$s .= '<b>' . $n . '</b></a>';
-			} else {
-				$s .= '<a href="./index.php?m=' . $m . '&amp;page=' . $n . '">' . $n . '</a>';
-			}
-			if ($n >= 30 + $page - 15) {
-				$xpg_break = true;
-				break;
-			} elseif ($n < $xpg_total_pages) {
-				$s .= ' | ';
-			}
-		}
-
-		if (!isset($xpg_break)) { // are we supposed to break ?
-			if ($n == $page) {
-				$s .= '<' . $n . '</a>';
-			} else {
-				$s .= '<a href="./index.php?m=' . $m . '&amp;page=' . $xpg_total_pages . '">';
-				$s .= $n . '</a>';
-			}
-		}
-		$s .= ' ] </td></tr>';
-	} else { // or we dont have any files..
-		$s .= '<td align="center">';
-		if ($xpg_next_page > $xpg_total_pages) {
-			$s .= $xpg_sqlrecs . ' ' . $m . ' ';
-		}
-		$s .= '</td></tr>';
-	}
-	$s .= '</table>';
-	echo $s;
-}
-
 $tab = $AppUI->getState('NoteIdxTab') !== null ? $AppUI->getState('NoteIdxTab') : 0;
 $page = (int) w2PgetParam($_GET, 'page', 1);
 $search = w2PgetParam($_REQUEST, 'search', '');
@@ -104,9 +19,6 @@ if (!isset($project_id)) {
 if (!isset($showCompany)) {
 	$showCompany = true;
 }
-
-$xpg_pagesize = 30;
-$xpg_min = $xpg_pagesize * ($page - 1); // This is where we start our record set from
 
 $project = new CProject();
 $task = new CTask();
@@ -167,19 +79,15 @@ $project->setAllowedSQL($AppUI->user_id, $q, 'note_project');
 $task->setAllowedSQL($AppUI->user_id, $q, 'note_task and task_project = note_project');
 $q->addOrder('company_name, note_title');
 
-//LIMIT ' . $xpg_min . ', ' . $xpg_pagesize ;
-//if ($canRead) {
-	$notes = $q->loadList();
-//} else {
-//	$AppUI->redirect('m=public&a=access_denied');
-//}
-// counts total recs from selection
-$xpg_totalrecs = count($notes);
+$items = $q->loadList();
 
-// How many pages are we dealing with here ??
-$xpg_total_pages = ($xpg_totalrecs > $xpg_pagesize) ? ceil($xpg_totalrecs / $xpg_pagesize) : 0;
+$xpg_pagesize = w2PgetConfig('page_size', 50);
+$xpg_min = $xpg_pagesize * ($page - 1); // This is where we start our record set from
+$xpg_totalrecs = count($items);
+$items = array_slice($items, $xpg_min, $xpg_pagesize);
 
-shownavbar_notes($xpg_totalrecs, $xpg_pagesize, $xpg_total_pages, $page);
+$pageNav = buildPaginationNav($AppUI, $m, $tab, $xpg_totalrecs, $xpg_pagesize, $page);
+echo $pageNav;
 
 ?>
 <table width="100%" border="0" cellpadding="2" cellspacing="1" class="tbl">
@@ -198,7 +106,7 @@ $fp = -1;
 
 $id = 0;
 for ($i = ($page - 1) * $xpg_pagesize; $i < $page * $xpg_pagesize && $i < $xpg_totalrecs; $i++) {
-	$row = $notes[$i];
+	$row = $items[$i];
 	$note_created = new CDate($row['note_created']);
 
 	if ($fp != $row['note_company']) {
@@ -244,4 +152,5 @@ for ($i = ($page - 1) * $xpg_pagesize; $i < $page * $xpg_pagesize && $i < $xpg_t
 <?php } ?>
 </table>
 <?php
-shownavbar_notes($xpg_totalrecs, $xpg_pagesize, $xpg_total_pages, $page);
+
+echo $pageNav;
