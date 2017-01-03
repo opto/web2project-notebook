8672df0cbd891921ae1406816a88f0a619b7f702
Keith Casey <keith@caseysoftware.com>
2017-01-03 00:13:43 -0600

    cleaning up the query

--- notebook/index_table.php
+++ notebook/index_table.php
@@ -26,12 +26,6 @@ $task = new CTask();
 $df = $AppUI->getPref('SHDATEFORMAT');
 $tf = $AppUI->getPref('TIMEFORMAT');
 
-if ($tab <= 0) {
-	$catsql = '';
-} else {
-	$catsql = 'note_category = ' . --$tab;
-}
-
 // SETUP FOR NOTE LIST
 $q = new w2p_Database_Query();
 $q->addQuery('notes.*');
@@ -52,10 +46,10 @@ if (!empty($search)) {
 	$q->addWhere('(note_title LIKE \'%' . $search . '%\' OR note_body LIKE \'%' . $search . '%\')');
 }
 if ($company_id) { // Company
-	$q->addWhere('(note_company = ' . (int)$company_id . ' OR note_company = 0)');
+	$q->addWhere('(note_company = ' . (int)$company_id . ')');
 }
 if ($project_id) { // Project
-	$q->addWhere('(note_project = ' . (int)$project_id . ' OR note_project = 0)');
+	$q->addWhere('(note_project = ' . (int)$project_id . ')');
 }
 if ($task_id) { // Task
 	$q->addWhere('note_task = ' . (int)$task_id);
@@ -69,12 +63,9 @@ if (isset($note_status) && $note_status >= 0) { // Task
 
 $q->addWhere('(note_private = 0 OR note_creator = ' . (int)$AppUI->user_id . ')');
 
-if ($catsql) { // Category
-	$q->addWhere($catsql);
-}
 // Permissions
 $project->setAllowedSQL($AppUI->user_id, $q, 'note_project');
-$task->setAllowedSQL($AppUI->user_id, $q, 'note_task and task_project = note_project');
+$task->setAllowedSQL($AppUI->user_id, $q, 'note_task');
 $q->addOrder('company_name, note_title');
 
 $items = $q->loadList();
